196f0b3c8a389e20f4453c1fd01b8ce6960e4138
jeff_han <djefry.h@gmail.com>
2024-07-27 18:45:39 +0700

    feat: python dap ui fix - work for single file

--- after/plugin/debug.lua
+++ after/plugin/debug.lua
@@ -1,16 +1,37 @@
-local dap_python require('dap-python').setup("/home/wandashare/.pyenv/versions/debugpy/bin/python")
+require('dap-python').setup("/home/wandashare/.pyenv/versions/debugpy/bin/python")
+-- Custom setum to run debug
+-- table.insert(require('dap').configurations.python, {
+--   type = 'python',
+--   request = 'launch',
+--   name = 'My Configurations',
+--   program = "flask",
+--   args = {"run", "--eager-load"}
+-- })
+
+require('nvim-dap-projects').search_project_config()
 local dap, dapui = require("dap"), require("dapui")
-dap.listeners.after.event_initialized["dapui_config"] = function()
+dapui.setup()
+
+dap.listeners.before.attach.dapui_config = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
   dapui.close()
 end
-dap.listeners.before.event_exited["dapui_config"] = function()
+dap.listeners.before.event_exited.dapui_config = function()
   dapui.close()
 end
 
-require('nvim-dap-projects').search_project_config()
--- vim.keymap.set("n", "<leader>dn", require('dap-python').test_method())
--- vim.keymap.set("n", "<leader>df", require('dap-python').test_class())
--- vim.keymap.set("v", "<leader>ds", require('dap-python').debug_selection())
+-- Make the breakpoints look nicer
+vim.fn.sign_define('DapBreakpoint',{ text ='🟥', texthl ='', linehl ='', numhl =''})
+vim.fn.sign_define('DapStopped',{ text ='▶️', texthl ='', linehl ='', numhl =''})
+
+-- Debug key mapping, can use this or click the button on nvim-dap-gui
+vim.keymap.set('n', '<F5>', dap.continue)
+vim.keymap.set('n', '<F6>', dap.step_over)
+vim.keymap.set('n', '<F7>', dap.step_into)
+vim.keymap.set('n', '<F8>', dap.step_out)
+vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
