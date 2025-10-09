d8708ca2762af8448c91589a47f6032554e26dca
jeff_han <djefry.h@gmail.com>
2025-10-09 20:22:22 +0000

    feat: update lsp config to use new api

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,9 +1,46 @@
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {
-    'marksman', 'lua_ls'
-  },
+require("mason").setup()
+require("mason-lspconfig").setup({
+  ensure_installed = { 'marksman', 'lua_ls'},
+  automatic_installation = true,
   handlers = {},
+})
+
+-- Nvim-cmp setup
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+cmp.setup({
+  snippet = {
+    expand = function(args) luasnip.lsp_expand(args.body) end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+    { name = "path" },
+  }),
 })
 
 vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
@@ -20,3 +57,15 @@ vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 vim.diagnostic.config({
     virtual_text = true
 })
+
+-- Lua LSP config
+vim.lsp.config["lua_ls"] = {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = { version = "LuaJIT" },
+      diagnostics = { globals = { "vim" } },
+      workspace = { checkThirdParty = false },
+    },
+  },
+}
