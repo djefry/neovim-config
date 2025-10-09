da2ea8c7624e497947403294c630dfec8d39dcbd
jeff_han <djefry.h@gmail.com>
2025-10-09 20:17:21 +0000

    feat: update telescope

--- lua/wandashare/vimplug.lua
+++ lua/wandashare/vimplug.lua
@@ -4,7 +4,7 @@ vim.call('plug#begin', '~/.config/nvim/plugged')
 
 -- telescome for fuzzy finder
 Plug 'nvim-lua/plenary.nvim'
-Plug ('nvim-telescope/telescope.nvim', { tag = '0.1.4' })
+Plug ('nvim-telescope/telescope.nvim', { tag = '0.1.8' })
 -- or                                , { branch = '0.1.x' }
 
 -- nvim colorscheme
